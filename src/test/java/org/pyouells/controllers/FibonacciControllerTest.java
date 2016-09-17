7f1c0018df4e4e7ee70eb42c19ca5b20067e19f2
pyouells <patrick.youells@libertyits.com>
2016-09-16 22:53:42 -0400

    Updated unit tests

--- src/test/java/org/pyouells/controllers/FibonacciControllerTest.java
+++ src/test/java/org/pyouells/controllers/FibonacciControllerTest.java
@@ -1,7 +1,39 @@
 package org.pyouells.controllers;
 
-/**
- * Created by pyouells on 9/16/16.
- */
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class FibonacciControllerTest {
+
+    @Autowired
+    private FibonacciController fibonacciController;
+
+    @Mock
+    HttpServletRequest request;
+
+    @Test
+    public void getFibonacciSequence() throws Exception {
+        String response = fibonacciController.getFibonacciValues(13, request);
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(response, "The fibonacci series of 13 is: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233");
+    }
+
+    @Test
+    public void getFibonacciSequence2() throws Exception {
+        String response = fibonacciController.getFibonacciValues(15, request);
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(response, "The fibonacci series of 15 is: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610");
+    }
 }
--- src/test/java/org/pyouells/controllers/PalindromeControllerTest.java
+++ src/test/java/org/pyouells/controllers/PalindromeControllerTest.java
@@ -1,7 +1,41 @@
 package org.pyouells.controllers;
 
-/**
- * Created by pyouells on 9/16/16.
- */
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PalindromeControllerTest {
+
+    @Autowired
+    private PalindromeController palindromeController;
+
+    @Mock
+    HttpServletRequest request;
+
+    @Test
+    public void palindromeTest() throws Exception {
+        String response = palindromeController.getPalindrome("Race car", request);
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(response, "'Race car' is a Palindrome");
+
+    }
+
+    @Test
+    public void palindromeTest2() throws Exception {
+        String response = palindromeController.getPalindrome("Race cars", request);
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(response, "'Race cars' is not a Palindrome");
+
+    }
 }
